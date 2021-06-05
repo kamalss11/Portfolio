e0eebdb249b7d6e0a48c2624d396487188877a06
Kamalesh S <73590020+kamalss11@users.noreply.github.com>
2021-06-06 00:08:14 +0530

    Update resume.js

--- Js/resume.js
+++ Js/resume.js
@@ -82,3 +82,18 @@ $('#contact').submit((e)=>{
         },
     });
 })
+
+// OFF developer tools
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        return false;
+    }
+});
+
+// Right click > Inspect
+
+$(document).on("contextmenu", function (e) {        
+    e.preventDefault();
+});
